92aa8b9b8bb0a5b5cad2118ea6548835e0d3515c
wangmaoxiong <2268461750@qq.com>
2020-05-15 21:11:19 +0800

    @EnableConfigServer：开启 Spring cloud config server 配置服务器

--- src/main/java/com/wmx/springcloudconfigservermicros/SpringCloudConfigServerMicrosApplication.java
+++ src/main/java/com/wmx/springcloudconfigservermicros/SpringCloudConfigServerMicrosApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @EnableConfigServer：开启 Spring cloud config server 配置服务器
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerMicrosApplication {
